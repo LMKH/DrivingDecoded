cbf7862816b76ef7457474a13288f2eea17e823e
LMKH <lkavanaghp@gmail.com>
2026-02-21 15:15:35 +0000

    security updates

--- my_project/settings.py
+++ my_project/settings.py
@@ -24,7 +24,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure--m^c@varb+#0&wf2km@!0=^t1v^4-cc*(dk+y4h1svkh98fnl9'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
